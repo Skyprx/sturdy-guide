2e045f9cd381072077a6255df606d744c9f1822b
Mustapha BARKI <startexpor@gmail.com>
2024-09-27 09:54:27 +0100

    Initialize 6639-pass-args-first-by-default

---
+++ examples/vue-kitchen-sink/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../src/stories/**/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-storysource',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-a11y',
+  ],
+};
