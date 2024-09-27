81945064ed63acd40ad904c8230a9c72cd523e22
Mustapha BARKI <startexpor@gmail.com>
2024-09-27 09:53:33 +0100

    Initialize fix/ie11

---
+++ addons/docs/src/frameworks/common/config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable-next-line import/no-extraneous-dependencies */
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
+
+export const parameters = {
+  docs: {
+    container: DocsContainer,
+    page: DocsPage,
+  },
+};
