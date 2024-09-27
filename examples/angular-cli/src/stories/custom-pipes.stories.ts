67144cc95824d39859e4d18a5492faede63f6451
Mustapha BARKI <startexpor@gmail.com>
2024-09-27 09:41:42 +0100

    Initialize with-all-fixes

---
+++ examples/angular-cli/src/stories/custom-pipes.stories.ts
@@ -0,0 +1,40 @@
+import { moduleMetadata } from '@storybook/angular';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
+import { NameComponent } from './moduleMetadata/name.component';
+import { CustomPipePipe } from './moduleMetadata/custom.pipe';
+
+export default {
+  title: 'Custom|Pipes',
+  decorators: [
+    moduleMetadata({
+      imports: [],
+      schemas: [],
+      declarations: [CustomPipePipe],
+      providers: [],
+    }),
+  ],
+};
+
+export const simple = () => ({
+  component: NameComponent,
+  props: {
+    field: 'foobar',
+  },
+});
+
+simple.story = {
+  name: 'Simple',
+};
+
+export const withKnobsStory = () => ({
+  component: NameComponent,
+  props: {
+    field: text('field', 'foobar'),
+  },
+});
+
+withKnobsStory.story = {
+  name: 'With Knobs',
+  decorators: [withKnobs],
+};
