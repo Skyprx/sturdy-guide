ae6413adbf9b758e84fd722774f4111205d2f528
Mustapha BARKI <startexpor@gmail.com>
2024-09-27 10:00:05 +0100

    Initialize dependabot/npm_and_yarn/protractor-7.0.0

---
+++ examples/vue-kitchen-sink/src/stories/addon-backgrounds.stories.js
@@ -0,0 +1,36 @@
+export default {
+  title: 'Addon/Backgrounds',
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'light', value: '#eeeeee' },
+        { name: 'dark', value: '#222222' },
+      ],
+    },
+  },
+};
+
+export const Story1 = () => {
+  const content = 'You should be able to switch backgrounds for this story';
+
+  return {
+    template: `<button>${content}</button>`,
+  };
+};
+
+Story1.story = {
+  name: 'story 1',
+};
+
+export const Story2 = () => {
+  const content = 'This one too!';
+
+  return {
+    template: `<button>${content}</button>`,
+  };
+};
+
+Story2.story = {
+  name: 'story 2',
+};
